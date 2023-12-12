624f102462d5c527fdf37a6753c2f9f73caf1581
SripaliManimekala <sripalimanimekala@gmail.com>
2023-12-12 19:41:28 +0530

    home section content and style

---
+++ main.js
@@ -0,0 +1,7 @@
+ var typed= new Typed(".text", {
+    // strings: ["fronted developer", "Youtuber", "web developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+ });
